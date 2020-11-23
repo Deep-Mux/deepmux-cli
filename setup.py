e67cd4d537bdf842331dadd1698e931b309fb398
MihanixA <mihanix.it@gmail.com>
2020-11-23 21:17:16 +0300

    bump version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="deepmux-cli",
-    version="0.0.1",
+    version="0.0.2",
     author="DeepMux",
     author_email="dev@deepmux.com",
     long_description=long_description,
