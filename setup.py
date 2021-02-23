37332ea9a50635e5a8d1de698423ca80abaa9b99
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2021-02-23 19:11:24 +0300

    0.0.12 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="deepmux-cli",
-    version="0.0.11",
+    version="0.0.12",
     author="DeepMux",
     author_email="dev@deepmux.com",
     long_description=long_description,
